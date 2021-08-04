608f194b0b22794584484226f3af73b7869bfe46
James Davidson (davids44) <Jamesdvdavidson@gmail.com>
2021-08-04 16:28:07 +0100

    Transfer from edu Github

--- src/helpers/data/serviceCheckBoxes.js
+++
@@ -1,6 +0,0 @@
-import {Services} from './ServiceTaxonomiesForOlderPeople';
-import {map} from 'lodash';
-
-let serviceCheckBoxes = map(Services.taxonomies, "name").sort();
-
-export default serviceCheckBoxes;
